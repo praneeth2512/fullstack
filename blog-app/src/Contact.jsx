14350ba520da18def01051e77e3768a379a86cc9
praneeth <234g1a33c3@srit.ac.in>
2025-09-16 13:53:09 +0530

    initial commit

--- blog-app/src/Contact.jsx
+++ blog-app/src/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
       <div>
-        <h1>mama</h1>
+        <h1> Page</h1>
         <p>This is the contact page of the blog application.</p>
       </div>
     );
